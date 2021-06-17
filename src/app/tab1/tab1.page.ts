a67a06d58046776b1eff7a7e91abc21d32625f53
mohanedbenmansour <mohaned.benmansour@esprit.tn>
2021-06-17 08:47:34 +0100

    picking random color

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,14 +8,29 @@ import { ColorChangerService } from '../services/color-changer.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  toolbarColor:string="primary";
+  toolbarColor:string="light";
+  contentColor:string="light"
   constructor(    private activatedRoute: ActivatedRoute,) {
     this.activatedRoute.queryParams.subscribe(params => {
-      this.toolbarColor = params['color'];
-      console.log(this.toolbarColor); 
+      if(params['color']=="random"){
+        this.toolbarColor=this.getRandomColor();
+        this.contentColor=this.getRandomColor();
+      }else {
+           this.toolbarColor = params['color'];
+      
+      this.contentColor = params['color'];
+      }
+   
   });
   
   }
- 
+ getRandomColor(){
+
+    const myArray = ["primary", "dark", "secondary","tertiary","success","warning","light","medium"];    
+    const randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+    return randomElement
+
+  
+ }
 
 }
